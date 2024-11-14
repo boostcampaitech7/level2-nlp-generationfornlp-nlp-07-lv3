79ebfc1a919a42f0857bb7a947b463a335a2c473
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2024-11-14 19:02:08 +0900

    no message

--- src/main.py
+++ src/main.py
@@ -412,4 +412,4 @@ if __name__ == '__main__':
         while argv_run_name == '':
             argv_run_name = input("run name is missing, please add run name : ")
 
-    main(argv_run_name, debug=True)
+    main(argv_run_name)
