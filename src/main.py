ad4d2621bc2a39a25ac3d8a02acc77761387d289
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2024-11-22 12:17:36 +0900

    add: utils.py

--- src/main.py
+++ src/main.py
@@ -7,134 +7,29 @@ import torch
 import wandb
 import pandas as pd
 import numpy as np
-import random
 import evaluate
 
-from ast import literal_eval
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM, SFTConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser, TrainingArguments, BitsAndBytesConfig
 from datasets import Dataset
 from tqdm import tqdm
-from peft import AutoPeftModelForCausalLM, LoraConfig
-from arguments import ModelArguments, DataTrainingArguments, CustomArguments
+from peft import AutoPeftModelForCausalLM
 
+from arguments import ModelArguments, DataTrainingArguments, CustomArguments
+from utils import record_to_df, train_df_to_process_df, test_df_to_process_df, set_seed
+
+SEED = 42
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+parent_dir = os.path.dirname(os.getcwd())
+now = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
 
 pd.set_option('display.max_columns', None)
 os.environ["WANDB_MODE"] = "online"
 logger = logging.getLogger(__name__)
 
-parent_dir = os.path.dirname(os.getcwd())
-now = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
-
-def set_seed(seed: int = 42):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
-
-SEED = 42
-set_seed(SEED)
-DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-def record_to_df(dataset):
-    records = []
-    for _, row in dataset.iterrows():
-        problems = literal_eval(row['problems'])
-        record = {
-            'id': row['id'],
-            'paragraph': row['paragraph'],
-            'question': problems['question'],
-            'choices': problems['choices'],
-            'answer': problems.get('answer', None),
-            "question_plus": problems.get('question_plus', None),
-        }
-        # Include 'question_plus' if it exists
-        if 'question_plus' in problems:
-            record['question_plus'] = problems['question_plus']
-        records.append(record)
-
-    return pd.DataFrame(records)
-
-
-def train_df_to_process_df(dataset, q_plus, no_q_plus):
-    processed_dataset = []
-    for i in range(len(dataset)):
-        choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(dataset[i]["choices"])])
-
-        # <보기>가 있을 때
-        if dataset[i]["question_plus"]:
-            user_message = q_plus.format(
-                paragraph=dataset[i]["paragraph"],
-                question=dataset[i]["question"],
-                question_plus=dataset[i]["question_plus"],
-                choices=choices_string,
-            )
-        # <보기>가 없을 때
-        else:
-            user_message = no_q_plus.format(
-                paragraph=dataset[i]["paragraph"],
-                question=dataset[i]["question"],
-                choices=choices_string,
-            )
-
-        # chat message 형식으로 변환
-        processed_dataset.append(
-            {
-                "id": dataset[i]["id"],
-                "messages": [
-                    {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
-                    {"role": "user", "content": user_message},
-                    {"role": "assistant", "content": f"{dataset[i]['answer']}"}
-                ],
-                "label": dataset[i]["answer"],
-            }
-        )
-
-    return Dataset.from_pandas(pd.DataFrame(processed_dataset))
-
-
-def test_df_to_process_df(dataset, q_plus, no_q_plus):
-    test_dataset = []
-    for i, row in dataset.iterrows():
-        choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(row["choices"])])
-        len_choices = len(row["choices"])
-
-        # <보기>가 있을 때
-        if row["question_plus"]:
-            user_message = q_plus.format(
-                paragraph=row["paragraph"],
-                question=row["question"],
-                question_plus=row["question_plus"],
-                choices=choices_string,
-            )
-        # <보기>가 없을 때
-        else:
-            user_message = no_q_plus.format(
-                paragraph=row["paragraph"],
-                question=row["question"],
-                choices=choices_string,
-            )
-
-        test_dataset.append(
-            {
-                "id": row["id"],
-                "messages": [
-                    {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
-                    {"role": "user", "content": user_message},
-                ],
-                "label": row["answer"],
-                "len_choices": len_choices,
-            }
-        )
-
-    return test_dataset
-
-
 def main(run_name, debug=False):
+    set_seed(SEED)
+
     model_name = None
     parser = HfArgumentParser(
         (ModelArguments, DataTrainingArguments, TrainingArguments, CustomArguments)
---
+++ src/utils.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import random
+import numpy as np
+import torch
+
+from ast import literal_eval
+from datasets import Dataset
+
+def set_seed(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
+def record_to_df(dataset : pd.DataFrame) -> pd.DataFrame:
+    records = []
+    for _, row in dataset.iterrows():
+        problems = literal_eval(row['problems'])
+        record = {
+            'id': row['id'],
+            'paragraph': row['paragraph'],
+            'question': problems['question'],
+            'choices': problems['choices'],
+            'answer': problems.get('answer', None),
+            "question_plus": problems.get('question_plus', None),
+        }
+        # Include 'question_plus' if it exists
+        if 'question_plus' in problems:
+            record['question_plus'] = problems['question_plus']
+        records.append(record)
+
+    return pd.DataFrame(records)
+
+def train_df_to_process_df(dataset : pd.DataFrame, q_plus, no_q_plus) -> Dataset:
+    processed_dataset = []
+    for i in range(len(dataset)):
+        choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(dataset[i]["choices"])])
+
+        # <보기>가 있을 때
+        if dataset[i]["question_plus"]:
+            user_message = q_plus.format(
+                paragraph=dataset[i]["paragraph"],
+                question=dataset[i]["question"],
+                question_plus=dataset[i]["question_plus"],
+                choices=choices_string,
+            )
+        # <보기>가 없을 때
+        else:
+            user_message = no_q_plus.format(
+                paragraph=dataset[i]["paragraph"],
+                question=dataset[i]["question"],
+                choices=choices_string,
+            )
+
+        # chat message 형식으로 변환
+        processed_dataset.append(
+            {
+                "id": dataset[i]["id"],
+                "messages": [
+                    {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
+                    {"role": "user", "content": user_message},
+                    {"role": "assistant", "content": f"{dataset[i]['answer']}"}
+                ],
+                "label": dataset[i]["answer"],
+            }
+        )
+
+    return Dataset.from_pandas(pd.DataFrame(processed_dataset))
+
+def test_df_to_process_df(dataset : pd.DataFrame, q_plus, no_q_plus) -> list:
+    test_dataset = []
+    for i, row in dataset.iterrows():
+        choices_string = "\n".join([f"{idx + 1} - {choice}" for idx, choice in enumerate(row["choices"])])
+        len_choices = len(row["choices"])
+
+        # <보기>가 있을 때
+        if row["question_plus"]:
+            user_message = q_plus.format(
+                paragraph=row["paragraph"],
+                question=row["question"],
+                question_plus=row["question_plus"],
+                choices=choices_string,
+            )
+        # <보기>가 없을 때
+        else:
+            user_message = no_q_plus.format(
+                paragraph=row["paragraph"],
+                question=row["question"],
+                choices=choices_string,
+            )
+
+        test_dataset.append(
+            {
+                "id": row["id"],
+                "messages": [
+                    {"role": "system", "content": "지문을 읽고 질문의 답을 구하세요."},
+                    {"role": "user", "content": user_message},
+                ],
+                "label": row["answer"],
+                "len_choices": len_choices,
+            }
+        )
+
+    return test_dataset
